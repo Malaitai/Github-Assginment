a1401e727104873ecd328edfdcd66907f3a902c4
Mr Malaitai <49211523+Malaitai@users.noreply.github.com>
2020-06-11 09:38:17 +1200

    Added another function

--- assignment.py
+++ assignment.py
@@ -2,8 +2,13 @@ def adding_numbers(a, b):
   c = a
   return(c)
   
+def subtract_numbers(a, b):
+  # Either delete or comment out the 'continue' code and return (a-b)
+  continue
+  
   
 a = input("Enter a number:")
 b = input("Enter another number:")
 
+print(adding_numbers(a, b))
 print(adding_numbers(a, b))
